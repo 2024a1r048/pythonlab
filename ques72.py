692d36541bfd13cf826a3bc91926b75fabbf8fa1
2024a1r048 <2024a1r048@mietjammu.in>
2026-09-17 13:18:07 +0530

    Added new codes
    Implement a program to count occurrences of a number in a list.

---
+++ ques72.py
@@ -0,0 +1,13 @@
+#WAP to count how many times a particular elements appear in a list#
+
+numbers = [10,20,30,40,50]
+
+search = int(input("Enter number to count: "))
+
+count = 0
+
+for num in numbers:
+    if num == search:
+        count = count + 1
+
+print("Frequency:", count)
