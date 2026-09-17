58f89e64f257cd538828e000fc1cf7bd5063f9f1
2024a1r048 <2024a1r048@mietjammu.in>
2026-09-17 13:21:51 +0530

    Added new codes

---
+++ ques73.py
@@ -0,0 +1,12 @@
+#WAP to input two lists and create a third list containing common elements.#
+
+list1 = [10, 20, 30, 40]
+list2 = [30, 40, 50, 60]
+
+common = []
+
+for num in list1:
+    if num in list2:
+        common.append(num)
+
+print("Common elements:", common)
